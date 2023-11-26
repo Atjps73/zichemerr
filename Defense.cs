2d182fb28accd9ab275b883565aa9fef66254fc1
z1chemerr <135830226+Atjps73@users.noreply.github.com>
2023-11-26 11:42:56 +0500

    Add files via upload

--- Defense.cs
+++ Defense.cs
@@ -2,23 +2,32 @@ using UnityEngine;
 
 public class Defense : MonoBehaviour
 {
-    [SerializeField] private GameObject _hareInDoor;
-    [SerializeField] private GameObject _hareScreamer;
-    [SerializeField] private GameObject _doorOpen;
+    [SerializeField] private GameObject _RightDoorOpen;
+    [SerializeField] private GameObject _chicaDoor;
+    [SerializeField] private GameObject _chicaScreamer;
+
+    [SerializeField] private GameObject _leftDoorOpen;
+    [SerializeField] private GameObject _bonnieDoor;
+    [SerializeField] private GameObject _bonnieScreamer;
 
     private readonly int _attackDelay = 5;
 
     private void Update()
     {
-        if (_hareInDoor.activeSelf == true)
-            Invoke(nameof(AttackDelay), _attackDelay);
+        if (_bonnieDoor.activeSelf == true && _chicaDoor.activeSelf == true)
+            Invoke(nameof(ScreamerAnimotronic), _attackDelay);
     }
-    private void AttackDelay()
+
+    private void ScreamerAnimotronic()
     {
-        if (_doorOpen.activeSelf == true)
-        {
-            Destroy(_hareInDoor);
-            _hareScreamer.SetActive(true);
-        }
+        Attack—heck(_bonnieDoor, _bonnieScreamer, _leftDoorOpen);
+        Attack—heck(_chicaDoor, _chicaScreamer, _RightDoorOpen);
+    }
+
+    private void Attack—heck(GameObject animotronicDoor, GameObject animotronicScream, GameObject doorOpen)
+    {
+        if (doorOpen.activeSelf == true && animotronicDoor.activeSelf == true)
+            animotronicScream.SetActive(true);
     }
 }
+
